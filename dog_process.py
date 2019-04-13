6e2081261fb66be4239a8073e4e289f3bce78d7a
Yusuf Berkay Girgin <35403526+ybgirgin3@users.noreply.github.com>
2019-04-13 04:18:12 +0300

    Create dog_process.py

---
+++ dog_process.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Filename: dog_process.py
+"""
+Author: Yusuf Berkay Girgin
+date: 6 April 2019
+"""
+import json
+import requests
+from telegram.ext import Updater
+
+# api getter
+def get_url():
+    contents = requests.get("https://random.dog/woof.json").json()
+    url = contents['url']
+    return url
+
+def dog(bot, update):
+    get_url()
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+# End of dog_process.py
