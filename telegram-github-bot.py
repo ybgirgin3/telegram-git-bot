374ad8c84764b155bec6b12875a00f49dc1136ae
Yusuf Berkay Girgin <35403526+ybgirgin3@users.noreply.github.com>
2019-04-21 02:42:27 +0300

    Update telegram-github-bot.py

--- telegram-github-bot.py
+++ telegram-github-bot.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""
-Author: Yusuf Berkay Girgin
-date: 6 April 2019
-"""
-
+from help import *
+from hello import *
 from git_process import *
-from dog_process import *
 from telegram.ext import Updater, CommandHandler
-
-# makes you feel welcome
-def hello(bot, update):
-    name = update.message.from_user.first_name
-    update.message.reply_text("Hello Welcome {}".format(name))
+# fun shit
+from dog_process import *
 
 
 # main functions that makes other functions work
 def main():
-    updater = Updater("your telegram bot key")
+    updater = Updater('651372077:AAFCyxgIH2FX9s0WMtnbx1TtZtUV33ZqF48')
     dp = updater.dispatcher
 
     # handlers
     dp.add_handler(CommandHandler("hello", hello))
+    dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("dog", dog))
     dp.add_handler(CommandHandler("commands", commands))
     dp.add_handler(CommandHandler("name", name))
     dp.add_handler(CommandHandler("email", email))
+    dp.add_handler(CommandHandler("web_page", web_page))
+    dp.add_handler(CommandHandler("web_page_controller", web_page_controller))
     dp.add_handler(CommandHandler("git_repos", git_repos))
     dp.add_handler(CommandHandler("git_p_repos_count", git_p_repos_count))
     dp.add_handler(CommandHandler("git_p_gists_count", git_p_gists_count))
